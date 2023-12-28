8473ab67596f301ef1b26937b4915ac0257140b7
Gabriel Garcia <glggarcia2013@gmail.com>
2023-12-28 13:29:48 -0300

    Adiciona exemplo seeder e factory

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Client extends Model
@@ -22,7 +22,7 @@ class Client extends Model
         'birthdate' => 'datetime'
     ];
 
-    public function user(): HasOne
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
