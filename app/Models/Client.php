b1a0ccdc0bb9d9db04a467bba7d8ca06f738e486
Gabriel Garcia <glggarcia2013@gmail.com>
2023-12-28 15:34:40 -0300

    Ajuste Uuid

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,7 +11,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Client extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, HasUuids;
 
     protected $fillable = [
         'user_id',
@@ -21,6 +22,11 @@ class Client extends Model
     protected $casts = [
         'birthdate' => 'datetime'
     ];
+
+    public function uniqueIds(): array
+    {
+        return ['uuid'];
+    }
 
     public function user(): BelongsTo
     {
